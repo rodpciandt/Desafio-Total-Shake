491e70b021974ec7549f7f73130869eca985f219
rodp <rodrigops@ciandt.com>
2022-09-09 10:30:24 -0300

    adding tests

--- src/test/java/br/com/desafio/totalshake/service/ItemPedidoServiceTest.java
+++ src/test/java/br/com/desafio/totalshake/service/ItemPedidoServiceTest.java
@@ -1,7 +1,5 @@
 package br.com.desafio.totalshake.service;
 
-import br.com.desafio.totalshake.dto.ItemPedidoDTO;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,19 +9,5 @@ public class ItemPedidoServiceTest {
     @Autowired
     private ItemPedidoService service;
 
-    @Test
-    void test_create() {
-
-        var x = new ItemPedidoDTO();
-
-        x.setIdPedido(1L);
-        x.setDescricao("Description");
-        x.setQuantidade(3);
-
-        ItemPedidoDTO saved = service.createItem(x);
-
-        System.out.println(x);
-        System.out.println(saved);
-    }
 
 }
--- src/test/java/br/com/desafio/totalshake/service/PedidoServiceTest.java
+++ src/test/java/br/com/desafio/totalshake/service/PedidoServiceTest.java
@@ -1,10 +1,19 @@
 package br.com.desafio.totalshake.service;
 
 
+import br.com.desafio.totalshake.enums.Status;
+import br.com.desafio.totalshake.model.dto.request.PedidoRequest;
+import br.com.desafio.totalshake.model.dto.response.PedidoResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class PedidoServiceTest {
@@ -14,4 +23,68 @@ public class PedidoServiceTest {
     private PedidoService service;
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
+
+
+    private Long generatedId;
+
+    @BeforeEach
+    void beforeEach() {
+
+        var pedidoResponse = service.create(new PedidoRequest(Status.REALIZADO, null));
+
+        this.generatedId = pedidoResponse.getId();
+    }
+
+
+    @Test
+    void test_create() {
+        var pedidoResponse = service.create(new PedidoRequest(Status.REALIZADO, null));
+
+        assertNotNull(pedidoResponse);
+        assertEquals(Status.REALIZADO, pedidoResponse.getStatus());
+    }
+
+
+    @Test
+    void test_findAll() {
+
+        var pedidos = service.findAll();
+        log.info("Pedidos: " + pedidos);
+
+        pedidos.forEach(pedido -> {
+            assertNotNull(pedido.getId());
+        });
+
+    }
+
+    @Test
+    void test_findById() {
+        var pedido = service.findById(generatedId);
+        log.info("Pedido: " + pedido);
+        assertEquals(generatedId, pedido.getId());
+    }
+
+
+    @Test
+    void test_update() {
+
+        var oldPedido = service.findById(generatedId);
+        var newPedido = service.updateStatus(generatedId, Status.CANCELADO);
+
+
+        assertEquals(oldPedido.getId(), newPedido.getId());
+        assertNotEquals(oldPedido.getStatus(), newPedido.getStatus());
+        assertEquals(Status.CANCELADO, newPedido.getStatus());
+    }
+
+    @Test
+    void test_delete() {
+
+        service.delete(generatedId);
+
+        assertThrows(Exception.class, () -> service.findById(generatedId));
+    }
+
 }
+
+
